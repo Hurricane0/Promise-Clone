cf55996c905f2270f223b35f779a92c31390be76
Nikita <nikitadv777@gmail.com>
2020-08-09 15:05:55 +0300

    Then chain functionality

--- promise/promise.js
+++ promise/promise.js
@@ -1,9 +1,22 @@
 class MyPromise {
   constructor(executor) {
-    executor();
+    this.queue = [];
+
+    executor.call(null, this.onResolve, this.onReject);
   }
 
-  then() {}
+  onResolve(data) {
+    this.queue.forEach(callback => {
+      data = callback(data);
+    });
+  }
+
+  onReject() {}
+
+  then(fn) {
+    this.queue.push(fn);
+    return this;
+  }
   catch() {}
   finally() {}
 }
